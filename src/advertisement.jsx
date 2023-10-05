6883f3d692f50242adf8bb750a91a304bb2b5a86
CHIJ KUMAR SHRESTHA <cheese_sth@yahoo.com>
2023-10-05 10:43:49 -0500

    wendys add

---
+++ src/advertisement.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./advertisement.css";
+
+const Advertisement = () => {
+  return (
+    <div className="advertisement">
+      <img
+        src="https://s3-prod.adage.com/s3fs-public/styles/width_660/public/20210602_Wendys_Insitu_1.jpg"
+        alt="Advertisement"
+        height="300px"
+      />
+    </div>
+  );
+};
+
+export default Advertisement;
---
+++ src/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer">
+        <p>copyright of Wendys only</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
